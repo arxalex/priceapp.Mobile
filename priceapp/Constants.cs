680dff6f3305aee11b74d123b3d44966a0beffcb
Oleksandr Yushchenko <arxalex@arxalex.com>
2023-10-09 15:58:50 +0300

    fix default cords

--- priceapp/Constants.cs
+++ priceapp/Constants.cs
@@ -7,8 +7,8 @@ public class Constants
     public const int DefaultRadius = 1000;
     public const bool DefaultShowRussiaSupportBrandAlerts = false;
     public const bool DefaultUseCustomLocation = false;
-    public const double DefaultX = 50.4387577;
-    public const double DefaultY = 30.5229812;
+    public const double DefaultX = 30.5229812;
+    public const double DefaultY = 50.4387577;
     public static readonly string DefaultCustomLocation = $"{DefaultX};{DefaultY}";
     private const string DatabaseFilename = "PriceAppSQLite.db3";
     private const string CacheDatabaseFilename = "PriceAppSQLiteCache.db3";
